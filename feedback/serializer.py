40c87167ae92bc1a52f9de1ec472c2d4845121e7
Goldyn19 <nwaezegolden2002@gmail.com>
2024-11-20 13:17:13 +0100

    made image optional

--- feedback/serializer.py
+++ feedback/serializer.py
@@ -7,6 +7,8 @@ class FeedBackSerializer(serializers.ModelSerializer):
         model = FeedBack
         fields = '__all__'
 
+        image = serializers.ImageField(required=False, allow_null=True)
+
     def validate_email(self, value):
         if FeedBack.objects.filter(email=value).exists():
             raise serializers.ValidationError("A feedback entry with this email already exists.")
