0273decfb4bd89f98aba3e170f38b042fb142a02
Goldyn19 <nwaezegolden2002@gmail.com>
2024-11-20 20:06:30 +0100

    used postgresql

--- WorldBank/settings.py
+++ WorldBank/settings.py
@@ -88,17 +88,13 @@ WSGI_APPLICATION = 'WorldBank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST': config('DB_HOST'),
-    #     'PORT': config('DB_PORT', default='5432'),
-    # }
 }
 
 
