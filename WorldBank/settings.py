3eaa07e8936b90c52f414146a97576bdbe10b763
Goldyn19 <nwaezegolden2002@gmail.com>
2025-01-19 16:50:18 +0100

    added render to allowed host

--- WorldBank/settings.py
+++ WorldBank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'worldbank.onrender.com']
 
 
 # Application definition
