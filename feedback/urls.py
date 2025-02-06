8cd5b54e33d8cdfa4c1a9bc6664cdac568c3f560
Goldyn19 <nwaezegolden2002@gmail.com>
2025-02-06 11:55:03 +0100

    changed requirement.txt

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,5 +3,5 @@ from .views import FeedBackView, UpdateFeedbackImageView
 
 urlpatterns = [
     path('feedback', FeedBackView.as_view(), name='feedback'),
-path('feedback/update-image/<str:name>/', UpdateFeedbackImageView.as_view(), name='update-feedback-image'),
+ path('feedback/update-image/<str:name>/', UpdateFeedbackImageView.as_view(), name='update-feedback-image'),
 ]
