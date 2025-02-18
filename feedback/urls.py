e3fdda5afc9e28c908c6af1850e729e0afb1a790
Goldyn19 <nwaezegolden2002@gmail.com>
2025-02-18 09:20:52 +0100

    corrected feedback endpoint

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import FeedBackView, UpdateFeedbackImageView, FeedbackWithImageAPIView, FeedbackWithoutImageAPIView, FeedBackWithID
 
 urlpatterns = [
-    path('feedback<int:pk>', FeedBackView.as_view(), name='feedback'),
+    path('feedback', FeedBackView.as_view(), name='feedback'),
  path('feedback/update-image/<str:trainee_number>/', UpdateFeedbackImageView.as_view(), name='update-feedback-image'),
 path('feedback-with-image/', FeedbackWithImageAPIView.as_view(), name='feedback_with_image'),
 path('feedback-without-image/', FeedbackWithoutImageAPIView.as_view(), name='feedback_with_image'),
