ebb0893cebeb66a991b962b005b394b987515ff6
Goldyn19 <nwaezegolden2002@gmail.com>
2025-02-18 09:26:05 +0100

    corrected feedback endpoint

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,6 +3,7 @@ from .views import FeedBackView, UpdateFeedbackImageView, FeedbackWithImageAPIVi
 
 urlpatterns = [
     path('feedback', FeedBackView.as_view(), name='feedback'),
+path("feedback/<int:pk>/", FeedBackView.as_view(), name="feedback-detail"),
  path('feedback/update-image/<str:trainee_number>/', UpdateFeedbackImageView.as_view(), name='update-feedback-image'),
 path('feedback-with-image/', FeedbackWithImageAPIView.as_view(), name='feedback_with_image'),
 path('feedback-without-image/', FeedbackWithoutImageAPIView.as_view(), name='feedback_with_image'),
